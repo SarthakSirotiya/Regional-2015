8893cd11d75835e184466ef68d0863901d7d4c00
SarthakSirotiya <SarthakSSi2@gmail.com>
2018-05-03 14:45:02 -0500

    5/3/2018 @ 2:44 PM
    started the project and added a couple form objects

---
+++ Regional-2015/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Regional_2015
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filenamelabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // filenamelabel
+            // 
+            this.filenamelabel.AutoSize = true;
+            this.filenamelabel.Location = new System.Drawing.Point(13, 13);
+            this.filenamelabel.Name = "filenamelabel";
+            this.filenamelabel.Size = new System.Drawing.Size(75, 17);
+            this.filenamelabel.TabIndex = 0;
+            this.filenamelabel.Text = "File Name:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 493);
+            this.Controls.Add(this.filenamelabel);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Submission Statistics: Contestant #ABC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label filenamelabel;
+    }
+}
+
